a24c7a1b5407ee78cfc0211d3d5f40e9354a5ee4
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2018-11-02 05:43:25 -0500

    methods for adding metric data

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -3,6 +3,46 @@ import {View, Text} from 'react-native'
 import {getMetricMetaInfo} from '../utils/helpers'
 
 export default class AddEntry extends Component {
+    //state 
+    //   get data for the specific day 
+    state = {
+        run: 0,
+        bike: 0,
+        swim: 0,
+        sleep: 0,
+        eat: 0
+    }
+
+    incremement = (metric ) => {
+       const{max, step} = getMetricMetaInfo(metric)
+       this.setState((state)=> {
+           const count = state[metric] + step
+           return {
+               // prev state and current state merged 
+               ...state, 
+               [metric]: count > max ?  max : count 
+           }
+       })
+    }
+    decrement = (metric ) => {
+        this.setState((state)=> {
+            const count = state[metric] - getMetricMetaInfo(metric).step
+            return {
+                // prev state and current state merged 
+                ...state, 
+                [metric]: count <  0  ? 0 : count,
+            }
+        })
+     }
+
+     //  slide for sleep and eat 
+     slide = (metric, value) => {
+         this.setState(()=> ({
+             [metric]: value,
+             
+
+         }))
+     }
     render() {
         return (
             <View>
