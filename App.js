43e9c6d27e1885c6f4c189269044e12716e393e5
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2018-11-10 21:58:49 -0600

    removed comments

--- App.js
+++ App.js
@@ -70,54 +70,6 @@ const Tabs = TabNavigator({
   }
 })
 
-// const Tabs = createBottomTabNavigator(
-//   {
-//     History: History,
-//     AddEntry: AddEntry,
-//     Live: Live
-//   },
-//   {
-//     navigationOptions: ({ navigation }) => ({
-//       tabBarIcon: ({ tintColor }) => {
-//         const { routeName } = navigation.state;
-//         // You can return any component that you like here! We usually use an
-//         // icon component from react-native-vector-icons
-//         return routeName === 'History' ? (
-//           <Ionicons name="ios-bookmarks" size={30} color={tintColor} />
-//         ) : (
-//           <FontAwesome name="plus-square" size={30} color={tintColor} />
-//         );
-//       },
-//       style: {
-//         height: 56,
-        // backgroundColor: Platform.OS === 'ios' ? white : purple,
-    //     shadowColor: 'rgba(0, 0, 0, 0.24)',
-    //     shadowOffset: {
-    //       width: 0,
-    //       height: 3
-    //     },
-    //     shadowRadius: 6,
-    //     shadowOpacity: 1
-
-    //   },
-    // }),
-//     tabBarOptions: {
-//       showIcon: true,
-//       activeTintColor: Platform.OS === 'ios' ? purple : white,
-//       style: {
-//         height: 56,
-//         backgroundColor: Platform.OS === 'ios' ? white : purple,
-//         shadowColor: 'rgba(0, 0, 0, 0.24)',
-//         shadowOffset: {
-//           width: 0,
-//           height: 3,
-//         },
-//         shadowRadius: 6,
-//         shadowOpacity: 1,
-//       },
-//     },
-//   }
-// );
 
 
 
