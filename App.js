524b8e9f0a8af023ff8603af0ea06bd34d58825d
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2018-11-10 21:58:20 -0600

    routing complete

--- App.js
+++ App.js
@@ -2,71 +2,14 @@ import React from 'react';
 import {Ionicons, FontAwesome} from '@expo/vector-icons'
 import AddEntry from './components/AddEntry'
 import {createStore} from 'redux'
-import {createBottomTabNavigator} from 'react-navigation'
+import {createBottomTabNavigator, TabNavigator} from 'react-navigation'
 import {purple, white} from './utils/colors'
 import {Provider} from 'react-redux'
 import {constants} from 'expo'
 import reducer from './reducers'
 import History from './components/History'
-//   wrap everything in provider component and pass it  store and pass it 
-//   fix AddEntry vs addEntry 
-
-function UdaciStatusBar({backgroundColor, ...props}) {
-  return (
-    <View style={{backgroundColor, height: Constants.StatusBarHeight}}>
-      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
-    </View>
-  )
-}
-// fix this navigation and consult docs 
-const Tabs = createBottomTabNavigator(
-  {
-    History: History,
-    AddEntry: AddEntry,
-  },
-  {
-    navigationOptions: ({ navigation }) => ({
-      tabBarIcon: ({ tintColor }) => {
-        const { routeName } = navigation.state;
-        // You can return any component that you like here! We usually use an
-        // icon component from react-native-vector-icons
-        return routeName === 'History' ? (
-          <Ionicons name="ios-bookmarks" size={30} color={tintColor} />
-        ) : (
-          <FontAwesome name="plus-square" size={30} color={tintColor} />
-        );
-      },
-      style: {
-        height: 56,
-        backgroundColor: Platform.OS === 'ios' ? white : purple,
-        shadowColor: 'rgba(0, 0, 0, 0.24)',
-        shadowOffset: {
-          width: 0,
-          height: 3
-        },
-        shadowRadius: 6,
-        shadowOpacity: 1
-
-      },
-    }),
-    tabBarOptions: {
-      showIcon: true,
-      activeTintColor: Platform.OS === 'ios' ? purple : white,
-      style: {
-        height: 56,
-        backgroundColor: Platform.OS === 'ios' ? white : purple,
-        shadowColor: 'rgba(0, 0, 0, 0.24)',
-        shadowOffset: {
-          width: 0,
-          height: 3,
-        },
-        shadowRadius: 6,
-        shadowOpacity: 1,
-      },
-    },
-  }
-);
-
+import Live from './components/Live'
+import EntryDetail from './components/EntryDetail'
 
 import {
   Text,
@@ -81,6 +24,104 @@ import {
   Slider 
 } from 'react-native'
 
+//   wrap everything in provider component and pass it  store and pass it 
+//   fix AddEntry vs addEntry 
+
+function UdaciStatusBar({backgroundColor, ...props}) {
+  return (
+    <View style={{backgroundColor, height: Constants.StatusBarHeight}}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  )
+}
+// fix this navigation and consult docs 
+const Tabs = TabNavigator({
+  History: {
+    screen: History,
+    navigationOptions: {
+      tabBarLabel: 'History',
+      tabBarIcon: ({ tintColor }) => <Ionicons name='ios-bookmarks' size={30} color={tintColor} />
+    },
+  },
+  AddEntry: {
+    screen: AddEntry,
+    navigationOptions: {
+      tabBarLabel: 'Add Entry',
+      tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor} />
+    },
+  },
+}, {
+  navigationOptions: {
+    header: null
+  },
+  tabBarOptions: {
+    activeTintColor: Platform.OS === 'ios' ? purple : white,
+    style: {
+      height: 56,
+      backgroundColor: Platform.OS === 'ios' ? white : purple,
+      shadowColor: 'rgba(0, 0, 0, 0.24)',
+      shadowOffset: {
+        width: 0,
+        height: 3
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1
+    }
+  }
+})
+
+// const Tabs = createBottomTabNavigator(
+//   {
+//     History: History,
+//     AddEntry: AddEntry,
+//     Live: Live
+//   },
+//   {
+//     navigationOptions: ({ navigation }) => ({
+//       tabBarIcon: ({ tintColor }) => {
+//         const { routeName } = navigation.state;
+//         // You can return any component that you like here! We usually use an
+//         // icon component from react-native-vector-icons
+//         return routeName === 'History' ? (
+//           <Ionicons name="ios-bookmarks" size={30} color={tintColor} />
+//         ) : (
+//           <FontAwesome name="plus-square" size={30} color={tintColor} />
+//         );
+//       },
+//       style: {
+//         height: 56,
+        // backgroundColor: Platform.OS === 'ios' ? white : purple,
+    //     shadowColor: 'rgba(0, 0, 0, 0.24)',
+    //     shadowOffset: {
+    //       width: 0,
+    //       height: 3
+    //     },
+    //     shadowRadius: 6,
+    //     shadowOpacity: 1
+
+    //   },
+    // }),
+//     tabBarOptions: {
+//       showIcon: true,
+//       activeTintColor: Platform.OS === 'ios' ? purple : white,
+//       style: {
+//         height: 56,
+//         backgroundColor: Platform.OS === 'ios' ? white : purple,
+//         shadowColor: 'rgba(0, 0, 0, 0.24)',
+//         shadowOffset: {
+//           width: 0,
+//           height: 3,
+//         },
+//         shadowRadius: 6,
+//         shadowOpacity: 1,
+//       },
+//     },
+//   }
+// );
+
+
+
+
 export default class App extends React.Component {
 
 
@@ -89,8 +130,7 @@ export default class App extends React.Component {
     <Provider store={createStore(reducer)}>
       <View style={{flex: 1}}>
        <View style={{height: 20}} />
-       <History />
-        <AddEntry />
+       <Tabs />
        
       </View>
     </Provider>
